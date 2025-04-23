3aa391b54595c6d8590a85855b8f9e7799e8e86d
Dev Madlani <madlanidev@gmail.com>
2025-04-23 20:59:52 +0530

    use dynamic routing to delete and rename files

--- server/app.js
+++ server/app.js
@@ -1,17 +1,47 @@
 import express from "express";
-import { readdir } from "fs/promises";
+import { readdir, rename, rm } from "fs/promises";
 const app = express();
 app.use((req, res, next) => {
-  res.set("Access-Control-Allow-Origin", "*");
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers": "*",
+  });
   next();
 });
-app.use((req, res, next) => {
+app.use(express.json());
+//serving static files
+// app.use((req, res, next) => {
+//   if (req.query.action === "download") {
+//     res.set("Content-Disposition", `attachment; `);
+//   }
+//   express.static("./storage")(req, res, next);
+// });
+
+app.get("/:filename", (req, res) => {
+  const { filename } = req.params;
   if (req.query.action === "download") {
-    res.set("Content-Disposition", `attachment; `);
+    res.set("Content-Disposition", "attchment");
   }
-  express.static("./storage")(req, res, next);
+  res.sendFile(`${import.meta.dirname}/storage/${filename}`);
+});
+app.delete("/:filename", async (req, res) => {
+  const { filename } = req.params;
+  try {
+    await rm(`${import.meta.dirname}/storage/${filename}`);
+    res.json({ message: "File Deleted Successfully" });
+  } catch (error) {
+    res.status(404).json({ message: "File Not Found" });
+  }
 });
 
+app.patch("/:filename", async (req, res) => {
+  const { filename } = req.params;
+  await rename(`./storage/${filename}`, `./storage/${req.body.newFileName}`);
+  res.json({ message: "File Renamed Successfully" });
+});
+
+//serving directory content
 app.get("/", async (req, res) => {
   const fileList = await readdir("./storage");
   res.json(fileList);
