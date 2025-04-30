014a1768bd4f90ff4d911a37bfe70cb9fbd306ff
Dev Madlani <madlanidev@gmail.com>
2025-04-30 08:23:59 +0530

    created json file to store the file data

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/directory", directoryRoutes);
-app.use("/files", fileRoutes);
+app.use("/file", fileRoutes);
 
 //serving static files
 // app.use((req, res, next) => {
--- server/routes/fileRoutes.js
+++ server/routes/fileRoutes.js
@@ -1,29 +1,36 @@
 import express from "express";
 import { createWriteStream } from "fs";
-import { rename, rm } from "fs/promises";
+import { rename, rm, writeFile } from "fs/promises";
 import path from "path";
+import fileData from "../filesDB.json" with { type: "json" };
 
 const router = express.Router();
 
 //CREATE
-router.post("/*", (req, res) => {
-  const filePath = path.join("/", req.params[0]);
-  console.log(filePath);
-  const writeableStrem = createWriteStream(`./storage/${filePath}`);
+router.post("/:filename", (req, res) => {
+  const { filename } = req.params;
+  const extention = path.extname(filename);
+  const id = crypto.randomUUID();
+  const fullFileName = `${id}${extention}`;
+  const writeableStrem = createWriteStream(`./storage/${fullFileName}`);
   req.pipe(writeableStrem);
-  req.on("end", () => {
+  req.on("end", async() => {
+    fileData.push({ id, extention,name:filename });
+    await writeFile("./filesDB.json", JSON.stringify(fileData));
     res.json({ message: "File Uploaded Successfully" });
   });
 });
 
 //READ
 // wild card routing
-router.get("/*", (req, res) => {
-  const filePath = path.join("/", req.params[0]);
+router.get("/:id", (req, res) => {
+  const {id} = req.params 
+  const data = fileData.find((file)=> file.id === id)
   if (req.query.action === "download") {
     res.set("Content-Disposition", "attachment");
   }
-  res.sendFile(`${process.cwd()}/storage/${filePath}`, (err) => {
+  const fullFileName = `${id}${data.extention}`;
+  res.sendFile(`${process.cwd()}/storage/${fullFileName}`, (err) => {
     if (err) {
       res.json({ message: "file not found" });
     }
@@ -32,10 +39,15 @@ router.get("/*", (req, res) => {
 
 //DELETE
 
-router.delete("/*", async (req, res) => {
-  const filePath = path.join("/", req.params[0]);
+router.delete("/:id", async (req, res) => {
+  const {id} = req.params
+  const data= fileData.find((file)=> file.id === id)
+  const fullFileName = `${id}${data.extention}`;
+  console.log(fullFileName);
   try {
-    await rm(`${import.meta.dirname}/storage/${filePath}`, { recursive: true });
+    await rm(`./storage/${fullFileName}`, { recursive: true });
+    const newFileData = fileData.filter((file) => file.id !== id);
+    await writeFile("./filesDB.json", JSON.stringify(newFileData));
     res.json({ message: "File Deleted Successfully" });
   } catch (error) {
     res.status(404).json({ message: "File Not Found" });
@@ -43,9 +55,17 @@ router.delete("/*", async (req, res) => {
 });
 
 //UPDATE
-router.patch("/*", async (req, res) => {
-  const filePath = req.params[0];
-  await rename(`./storage/${filePath}`, `./storage/${req.body.newFileName}`);
+router.patch("/:id", async (req, res) => {
+  const {id} = req.params
+  const data = fileData.find((file)=> file.id === id)
+  console.log(data)
+  if(!data){
+    res.status(404).json({message : "File Not Found"})
+    return
+  }
+const fullFileName = `${id}${data.extention}`
+  await rename(`./storage/${fullFileName}`, `./storage/${req.body.newFileName}`);
+  console.log(fileData.find((file)=> file.id === id))
   res.json({ message: "File Renamed Successfully" });
 });
 
